1a5103981e8df86da6765d370f6750f9e12764c1
Alexander Klatte <42543605+ktxcx@users.noreply.github.com>
2020-10-21 09:11:28 +0200

    Update requirements.sh

--- k8s/requirements.sh
+++ k8s/requirements.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update && \
+apt-get upgrade -y && \
+apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
