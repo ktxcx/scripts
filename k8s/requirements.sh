83d87cd754a0fb86a6f7b35c12199ec80eb2a580
Alexander Klatte <42543605+ktxcx@users.noreply.github.com>
2021-03-04 22:21:58 +0100

    Update requirements.sh

--- k8s/requirements.sh
+++ k8s/requirements.sh
@@ -10,3 +10,14 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl ipvsadm jq
 kubeadm config images pull
+
+modprobe overlay
+modprobe br_netfilter
+
+tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
